5eeea76abe16057a19c35744865b516f67abd557
Gilcimar Tamanini <modal.upgt@gmail.com>
2024-11-26 00:29:08 -0300

    Adicionado cors para permitir conexões de urls pré definidas

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import { listarPosts, novoPost, uploadImagem, editarPost, editarGeminiPost } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,6 +21,7 @@ const storage = multer.diskStorage({
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(corsOptions);
 
     app.get("/api/posts", listarPosts);
 
